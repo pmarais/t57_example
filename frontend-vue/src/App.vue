207e6996fe46562a601417261a6ae44e53036f06
Philip Marais <philipmarais@gmail.com@users.noreply.github.com>
2022-05-18 16:28:07 +0200

    Adding some frontend styling

--- frontend-vue/src/App.vue
+++ frontend-vue/src/App.vue
@@ -1,17 +1,19 @@
 <template>
-  <Header title="This is hello world"/>
-  <Urls @delete-url="deleteUrl" :urls="urls"/>
+  <header class="text-center bg-blue-800 text-white p-4 mb-10">
+    <div class="text-3xl font-bold mb-3"> <i class="fa fa-viruses"></i> Trello Bookmarks</div>
+  </header>
+  <div>
+    <Urls @delete-url="deleteUrl" :urls="urls"/>
+  </div>
 </template>
 
 <script>
-import Header from './components/Header.vue'
 import Urls from './components/Urls.vue'
 import axios from 'axios'
 
 export default {
   name: 'App',
   components: {
-    Header,
     Urls,
   },
   data () {
@@ -39,66 +41,3 @@ export default {
   }
 }
 </script>
-
-<style>
-@import './assets/base.css';
-
-#app {
-  max-width: 1280px;
-  margin: 0 auto;
-  padding: 2rem;
-
-  font-weight: normal;
-}
-
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-a,
-.green {
-  text-decoration: none;
-  color: hsla(160, 100%, 37%, 1);
-  transition: 0.4s;
-}
-
-@media (hover: hover) {
-  a:hover {
-    background-color: hsla(160, 100%, 37%, 0.2);
-  }
-}
-
-@media (min-width: 1024px) {
-  body {
-    display: flex;
-    place-items: center;
-  }
-
-  #app {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    padding: 0 2rem;
-  }
-
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-}
-</style>
